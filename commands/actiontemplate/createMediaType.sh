e7bb773bc5734c220e127ba42ecd0e2044595d72
Severin Neumann <severin.neumann@altmuehlnet.de>
2018-12-03 16:52:34 +0100

    Add command to create media types for http request actions

---
+++ commands/actiontemplate/createMediaType.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+function actiontemplate_createmediatype {
+  apiCall -X POST -d '{"name":"${n}","builtIn":false}' '/controller/restui/httpaction/createHttpRequestActionMediaType' "$@"
+}
+
+register actiontemplate_createmediatype "Create a custom media type"
+
+describe actiontemplate_createmediatype << EOF
+Create a custom media type. Provide the name of the media type as parameter (-n)
+EOF
