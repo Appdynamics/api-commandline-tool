1184ebadb4c7f1efb50666ccff5b69661d1a51a5
Severin Neumann <severin.neumann@altmuehlnet.de>
2019-03-26 11:16:00 +0100

    Update for widget/dashboard time ranges for update_dashboard.sh

--- recipes/update_dashboard.sh
+++ recipes/update_dashboard.sh
@@ -10,7 +10,8 @@ DASHBOARD_ID=$2
 SINCE=${3:-${DEFAULT_SINCE}}
 UNTIL=${4:-${DEFAULT_UNTIL}}
 
+# Fix ADQL queries and set startTime and endTime for dashboard or widget specific time ranges
+JSON=$(../act.sh -E ${ENVIRONMENT} dashboard get -i ${DASHBOARD_ID} | sed -e "s/SINCE[[:space:]]*[0-9]*[[:space:]]*UNTIL[[:space:]]*[0-9]*/SINCE ${SINCE} UNTIL ${UNTIL}/g" | sed -e "s/\"\startTime\"\([[:space:]]*\):[^,]*,/\"startTime\"\1: ${SINCE},/g" | sed -e "s/\"\endTime\"\([[:space:]]*\):[^,]*,/\"endTime\"\1: ${UNTIL},/g")
 
-JSON=$(../act.sh -E ${ENVIRONMENT} dashboard get -i ${DASHBOARD_ID} | sed -e "s/SINCE[[:space:]]*[0-9]*[[:space:]]*UNTIL[[:space:]]*[0-9]*/SINCE ${SINCE} UNTIL ${UNTIL}/g")
 
 ../act.sh -E ${ENVIRONMENT} dashboard update -d "${JSON}"
